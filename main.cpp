13c4275ee36cb3df31bb976904f67af228389d71
MorisZheng <s04044mort@gmail.com>
2023-04-05 22:09:50 +0800

    update 4/5-3

--- main.cpp
+++ main.cpp
@@ -2,43 +2,30 @@
 #include "accelerometer.h"
 
 static BufferedSerial serial_port(USBTX, USBRX);
+DigitalOut led(LED1);
 
-InterruptIn btnRecord(BUTTON1);
-EventQueue queue(128 * EVENTS_EVENT_SIZE);
-Thread t;
 Accelerometer acc;
-double Accel[3]={0};
-double data[128][3];
-
-void record(void) {
-
-    for (int i = 0; i < 128; i++){
-        acc.GetAcceleromterSensor(Accel);
-        acc.GetAcceleromterCalibratedData(Accel);
-        data[i][0] = Accel[0];
-        data[i][1] = Accel[1];
-        data[i][2] = Accel[2];
-        ThisThread::sleep_for(10ms);
-    }
-}
-
-void printData()
-{
-    for (int i = 0; i < 128; i++){
-        printf("%lf/%lf/%lf\n" ,data[i][0], data[i][1], data[i][2]);
-    }
-}
+double data[128][3] = {{0}};
 
 int main() {
-    t.start(callback(&queue, &EventQueue::dispatch_forever));
     char buf;
+    led = 0;
     while (true){
         if (serial_port.readable()){
             serial_port.read(&buf, 1);
             if (buf == '1'){
-                record();
+                led = 1;
+                for (int i = 0; i < 128; i++){
+                    acc.GetAcceleromterSensor(data[i]);
+                    acc.GetAcceleromterCalibratedData(data[i]);
+                    ThisThread::sleep_for(10ms);
+                }
+                led = 0;
+                for (int i = 0; i < 128; i++){
+                    printf("%lf/%lf/%lf\n", data[i][0], data[i][1], data[i][2]);
+                }
             }
-            printData();
+            
         }
     }
 }
--- plot.py
+++ plot.py
@@ -6,14 +6,32 @@ from time import sleep
 Fs = 128.0;  # sampling rate
 Ts = 1280.0/Fs; # sampling interval
 t = np.arange(0,1280,Ts) # time vector; create Fs samples between 0 and 1.0 sec.
-y = np.arange(0,1280,Ts) # signal vector; create Fs samples
+x = np.arange(0,1280,Ts) # signal vector; create Fs samples
+y = np.arange(0,1280,Ts)
+z = np.arange(0,1280,Ts)
 
 serdev = 'COM3'
 s = serial.Serial(serdev)
 
-s.write(b'1')
-for i in range(0, int(Fs)):
-    line = s.readline()
-    line = line.strip(b'\t\n')
-    line = line.decode("utf-8")
-    print(line)
+print("Enter 'a' to start:")
+if ('a' == input("> ")):
+    print("start")
+    s.write(b'1')
+    sleep(0.001)
+    for i in range(0, int(Fs)):
+        line = s.readline()
+        line = line.strip(b'\r\n')
+        line = line.decode("utf-8")
+        data = line.split('/')
+        x[i] = float(data[0])
+        y[i] = float(data[1])
+        z[i] = float(data[2])
+    fig, ax = plt.subplots(3, 1)
+    ax[0].plot(t,x)
+    ax[0].set_xlabel('Time(ms)')
+    ax[1].plot(t,y)
+    ax[1].set_xlabel('Time(ms)')
+    ax[2].plot(t,z)
+    ax[2].set_xlabel('Time(ms)')
+    plt.show()
+s.close()
